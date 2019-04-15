00acefe11394bca75c8ee0835a0e4edaae1e810f
ZlayaAnia <a.buiakina@gmail.com>
2019-04-15 23:26:45 +0300

    Исправлен route для получения изображения и передачи его по следующим страницам. Подкорректирован HTML

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -34,12 +34,19 @@ def create_app():
     def uploaded_file(filename):
         return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
-    @app.route('/start')
+    @app.route('/start', methods=['GET', 'POST'])
     def start():
         if current_user.is_authenticated:
-            login_form = LoginForm()
-            time = datetime.now()
-            return render_template('start_work.html', form=login_form, time=time)
+            form = DownloadForm()
+            if request.method == 'POST':
+                user_id = User.query.filter(User.username == current_user.username).first()
+                filename = upload_file(current_user.id, form)
+                return redirect(url_for('.analise', file=filename))
+
+            
+            print(current_user.id)
+            
+            return render_template('start_work.html', form=form, )
         else:
             return redirect(url_for('index'))
 
@@ -104,35 +111,42 @@ def create_app():
             return redirect(url_for('login'))
         return redirect(url_for('login'))
 
+
     @app.route('/analise', methods=['GET', 'POST'])
     def analise():
         filename = None
         if current_user.is_authenticated:
+            
             title = 'TEST'
             form = DownloadForm()
             print(current_user.id)
-            user_id = User.query.filter(User.username == current_user.username).first()
-            filename = upload_file(current_user.id, form)
-            files_list = Files.query.order_by(Files.uploaded.desc()).all()
-            return render_template('analise.html', form=form, files_list=files_list, filename=filename, title=title)
+            if request.method == 'POST':
+                user_id = User.query.filter(User.username == current_user.username).first()
+                filename = upload_file(current_user.id, form)
+            else:
+                filename = request.args.get('file')
+            return render_template('analise.html', form=form, filename=filename, title=title)
+       
         else:
             return redirect(url_for('index'))
 
+    
 
-    @app.route('/projects', methods=['GET', 'POST'])
-    def projects():
-        if current_user.is_authenticated:
-            title = 'TEST'
-            form = ProjectsForm()
-            print(current_user.id)
-            experiment_time = datetime.now()
-            #db_data = Experiment(name='exp', image_scale='1', image_wb='2', image_cont='3', experiment_time=experiment_time, file_id='1')
-            files_list = Experiment.query.order_by(Experiment.experiment_time.desc()).all()
-            #db.session.add(db_data)
-            #db.session.commit()
-            return render_template('projects.html', form=form, files_list=files_list, title=title)
-        else:
-            return redirect(url_for('index'))
+
+    # @app.route('/projects', methods=['GET', 'POST'])
+    # def projects():
+    #     if current_user.is_authenticated:
+    #         title = 'TEST'
+    #         form = ProjectsForm()
+    #         print(current_user.id)
+    #         experiment_time = datetime.now()
+    #         #db_data = Experiment(name='exp', image_scale='1', image_wb='2', image_cont='3', experiment_time=experiment_time, file_id='1')
+    #         files_list = Experiment.query.order_by(Experiment.experiment_time.desc()).all()
+    #         #db.session.add(db_data)
+    #         #db.session.commit()
+    #         return render_template('projects.html', form=form, files_list=files_list, title=title)
+    #     else:
+    #         return redirect(url_for('index'))
 
 
 
