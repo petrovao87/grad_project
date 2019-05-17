8439a0675b1805e204f86d9d45e9593ef553fe33
petrovao87 <petrovao87@mail.ru>
2019-05-17 22:05:43 +0300

    refactor project, add blueprints

--- web_app/config.py
+++ web_app/config.py
@@ -5,4 +5,3 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'bmp'])
 SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = "YOUR_SECRET_KEY_HERE"
